a53f38fd380e765d19aa0ba276c8ad1077aced3e
parade岁月 <13703937803@163.com>
2022-12-08 08:45:23 +0800

    添加一一些正则

--- regex/commonReg.js
+++ regex/commonReg.js
@@ -1,7 +1,3 @@
 const digitalReg =  /^([1-9][\d]*|0)(\.[\d]+)?$/;//小数正则
-console.log(digitalReg.test('01'))//false
-console.log(digitalReg.test('1'))//true
-console.log(digitalReg.test('1.1'))//true
-console.log(digitalReg.test('1.1.1'))//false
-console.log(digitalReg.test('0.1'))//true
-console.log(digitalReg.test('1.0'))//true
+
+const phoneReg = /^1[3-9]\d{9}$///手机号正则
--- util/index.js
+++ util/index.js
@@ -1,10 +1,16 @@
-function getParamByUrl(url,name) {
+/**
+ * 计算url中的参数值
+ * @param url
+ * @param name 参数key
+ * @returns {string|null} 参数value
+ */
+function getParamByUrl(url, name) {
     // let reg = new RegExp(`[?&]${name}=[_a-zA-Z0-9%.-]*&?`)
     let reg = new RegExp(`[?&]${name}=[^#&]*&?`)
     let match = url.match(reg);
-    if (match){
-        return match[0].split("=")[1].replaceAll("&","")
-    }else{
+    if (match) {
+        return match[0].split("=")[1].replaceAll("&", "")
+    } else {
         return null
     }
 }
@@ -31,3 +37,58 @@ function keepTwoDecimalFull(num) {
     }
     return s_x;
 }
+
+function isValidDate(date) {
+    return date instanceof Date && !isNaN(date.getTime())
+}
+
+// 日期格式化
+function parseTime(time, pattern) {
+    if (arguments.length === 0 || !time) {
+        return null
+    }
+    const format = pattern || '{y}-{m}-{d} {h}:{i}:{s}'
+    let date
+    if (typeof time === 'object') {
+        date = time
+    } else {
+        if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+            time = parseInt(time)
+        } else if (typeof time === 'string') {
+            let originalTime = time
+            time = time.replace(new RegExp(/-/gm), '/').replace('T', ' ').replace(new RegExp(/\.[\d]{3}/gm), '');
+            let tempDate = new Date(time)
+            if (!isValidDate(tempDate)){
+                time = new Date(originalTime).getTime()+8*3600*1000
+            }
+        }
+        if ((typeof time === 'number') && (time.toString().length === 10)) {
+            time = time * 1000
+        }
+        date = new Date(time)
+    }
+    const formatObj = {
+        y: date.getFullYear(),
+        m: date.getMonth() + 1,
+        d: date.getDate(),
+        h: date.getHours(),
+        i: date.getMinutes(),
+        s: date.getSeconds(),
+        a: date.getDay()
+    }
+    const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+        let value = formatObj[key]
+        // Note: getDay() returns 0 on Sunday
+        if (key === 'a') {
+            return ['日', '一', '二', '三', '四', '五', '六'][value]
+        }
+        if (result.length > 0 && value < 10) {
+            value = '0' + value
+        }
+        return value || 0
+    })
+    return time_str
+}
+
+
+
